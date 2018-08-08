c03148f98d12cb1083f551f27f1b2d49b6733762
codyMalcolm <cody@codymalcolm.com>
2018-08-08 16:43:42 -0400

    add button functionality

--- scripts.js
+++ scripts.js
@@ -2,9 +2,31 @@ const controls = document.querySelector('.controls');
 const grid = document.querySelector('.display');
 const ctrlBtns = controls.querySelectorAll('.ctrl-btn');
 const width = grid.offsetWidth;
+let write = true;
+let mode = "black";
+
+function handleGridClick(e) {
+  if (e.target.classList.contains("cube")) write = !write;
+}
+
+window.addEventListener('click', handleGridClick);
 
 function handleClick(e) {
-  console.log(e.target.dataset.fun);
+  const selection = e.target.dataset.fun;
+  const size = window.prompt('Please enter a grid size:', 16);
+
+  switch (selection) {
+    case "0":
+      mode = "black";
+      break;
+    case "1":
+      mode = "color";
+      break;
+    case "2":
+      mode = "draw"
+      break;
+  }
+  isNaN(size) ? generateGrid() : generateGrid(parseInt(size));
 }
 
 ctrlBtns.forEach(b => {
@@ -14,6 +36,10 @@ ctrlBtns.forEach(b => {
 function generateGrid(x=16) {
   const div = document.createElement('div');
   div.classList.add('cube')
+
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
 
   for (let i = 0; i < x**2; i++) {
     grid.appendChild(div.cloneNode());
@@ -31,8 +57,35 @@ function generateListeners() {
   })
 }
 
-function handleMouseEnter(e) {
-  e.target.style.backgroundColor = "black";
+function generateColor() {
+  let hue = Math.floor(Math.random()*360);
+  let sat = Math.floor(Math.random()*101);
+  let ltn = Math.floor(Math.random()*81);
+  return `hsla(${hue}, ${sat}%, ${ltn}%, 1)`
 }
 
-generateGrid();
+function getNewColor(oldColor) {
+  if (!oldColor) return 'rgb(225, 225, 225)';
+  if (oldColor === 'rgb(0, 0, 0)') return oldColor;
+  const oldNum = oldColor.split(', ')[1];
+  const newNum = oldNum - 25;
+  return `rgb(${newNum}, ${newNum}, ${newNum})`
+}
+
+function handleMouseEnter(e) {
+  if (!write) return;
+  switch (mode) {
+    case "black":
+      e.target.style.backgroundColor = "black";
+      break;
+    case "color":
+      e.target.style.backgroundColor = generateColor();
+      break;
+    case "draw":
+      let newColor = getNewColor(e.target.style.backgroundColor);
+      e.target.style.backgroundColor = newColor;
+      break;
+  }
+}
+
+generateGrid(8);
